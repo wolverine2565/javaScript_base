8cff516f0f826325a6a8dbae8791b2fbff1b922a
Nikolay Danyukin <nikolaydanyukin@MacBook-Air-Nikolay.local>
2024-02-17 02:22:21 +0700

    3 задание

--- hw1/main.js
+++ hw1/main.js
@@ -1,3 +1,88 @@
-let personName = prompt(`Введи имя`);
-let personAge = prompt(`Введи возраст`);
-console.log(personName, personAge)
+
+
+// function helloName(name) {
+//     console.log(name);
+// }
+
+// helloName('Alex');
+
+// let count = 5;
+
+// function counter() {
+//     return count++;
+// }
+
+// counter();
+// counter();
+// counter();
+
+// console.log(count);  
+
+// let age = Number(prompt('Сколь вам лет?'))
+
+// function upAge() {
+//     return age + 5;
+// }
+// console.log(`Через 5 лет вам будет ${upAge(age)}`);
+
+// const lvlUpAge = () => age + 5;
+
+
+// console.log(`Через 5 лет вам будет ${lvlUpAge()}`);
+
+// function hello() {
+//     console.log('Hello world')
+// }
+
+// hello();
+
+// const sum = (param1, param2) => {
+//     return param1 + param2;
+// }
+
+// const result = sum(2,5);
+
+// console.log(result);
+
+
+// const salary = (money) => {
+//     money = money * 0.87;
+//     money = money * 0.75;
+//     return money;
+// }
+
+// console.log(salary(100));
+
+// function buy () {
+//     alert(`Вы нажали кнопку "купить"`)
+// }
+// const userAnswer = prompt(`Зимой и летом одним цветов`);
+
+// if (userAnswer === 'елка') {
+//     console.log (`Угадал`)    
+// } else {
+//     console.log(`Не угадал`)
+// }
+
+// const userAnswer2 = prompt(`Сидит дед во 100 шуб одет, кто его раздевает, тот слезы проливает`);
+
+// if (userAnswer === 'лук') {
+//     console.log (`Угадал`)    
+// } else {
+//     console.log(`Не угадал`)
+// }
+
+// function askQuestion(answer, question) {
+//     const userAnswer = prompt(question);
+//     if (userAnswer.toLocaleLowerCase() === answer) {
+//         alert(`Угадал`);  
+//     } else {
+//         alert(`Не угадал`);
+//     }
+// }
+
+
+// function puzzle() {
+//     askQuestion('лук', 'Сидит дед во 100 шуб одет, кто его раздевает, тот слезы проливает');
+//     askQuestion('елка', 'Зимой и летом одним цветов');
+// }
